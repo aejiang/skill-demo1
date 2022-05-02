a20644f6339cdf827817aa7271c6f3862b48dc93
aerny-jiang <aernyjiang@gmail.com>
2022-05-01 19:56:58 -0700

    added add method and test

--- skillsDemo1.java
+++ skillsDemo1.java
@@ -1,3 +1,6 @@
-public skillsDemo1 {
+public class skillsDemo1 {
 
+    public static int add(int int1, int int2) {
+        return int1 + int2;
+    }
 }
---
+++ skillsDemo1Test.java
@@ -0,0 +1,9 @@
+import static org.junit.Assert.*;
+import org.junit.*;
+
+public class skillsDemo1Test {
+    @Test 
+    public void addTest() {
+        assertEquals(5, skillsDemo1.add(2, 1));
+    }
+}
